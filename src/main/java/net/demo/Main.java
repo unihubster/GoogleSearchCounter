36cebfb31ecddbd4f9676753297f436e9838d9d4
unihubster <var@var>
2020-06-01 11:57:41 +0300

    auto mode for search

--- src/main/java/net/demo/Main.java
+++ src/main/java/net/demo/Main.java
@@ -2,10 +2,25 @@ package net.demo;
 
 import net.demo.utils.SearchRunner;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        new SearchRunner().runApp();
+//        new SearchRunner().runAppWithView();
+
+        List<String> queries = new LinkedList<>();
+        queries.add("java jsoup");
+        queries.add("java");
+        queries.add("jsoup");
+        queries.add("java jsoup test");
+        queries.add("selenium");
+        queries.add("cucumber");
+
+        Map<String, Integer> queriesSearchResultCounts = new SearchRunner().runAppInAutoMode(queries);
+        queriesSearchResultCounts.forEach((query, count) -> System.out.println(query + "=" + count));
     }
 
 }
--- src/main/java/net/demo/utils/SearchRunner.java
+++ src/main/java/net/demo/utils/SearchRunner.java
@@ -3,12 +3,15 @@ package net.demo.utils;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SearchRunner {
     private final ConsoleView view = new ConsoleView();
     private final OnlineSearchProcessor onlineSearchProcessor = new OnlineSearchProcessor();
 
-    public void runApp() {
+    public void runAppWithView() {
         switch (view.getAppModeFromUser()) {
             case (2):
                 runLinkGoogleSearch();
@@ -16,6 +19,28 @@ public class SearchRunner {
             default:
                 runWordsGoogleSearch();
         }
+    }
+
+    public Map<String, Integer> runAppInAutoMode(List<String> queries) {
+        Map<String, Integer> searchCounts = new TreeMap<>();
+        for (String query : queries) {
+            try {
+                String link = onlineSearchProcessor.prepareGoggleLink(query);
+                view.displayText(link);
+                Document document = onlineSearchProcessor.getPage(link);
+                searchCounts.put(query, onlineSearchProcessor.parseGoogleSearchResultsCount(document));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return searchCounts;
+    }
+
+    public int runAppInAutoMode(String query) throws IOException {
+        String link = onlineSearchProcessor.prepareGoggleLink(query);
+        view.displayText(link);
+        Document document = onlineSearchProcessor.getPage(link);
+        return onlineSearchProcessor.parseGoogleSearchResultsCount(document);
     }
 
     public void runLinkGoogleSearch() {
